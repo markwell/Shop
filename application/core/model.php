9855573a76ded1cde2295532fa5dde3ecda4b15c
Mark <mrak.mk@ya.ru>
2015-01-06 04:38:49 +1000

    Переношу подключение в базовый файл модели

--- application/core/model.php
+++ application/core/model.php
@@ -2,6 +2,19 @@
 
 class Model
 {
+	public function db_connect()
+	{
+		   # Соединямся с БД
+		try {  
+          $DBH = new PDO("mysql:host=localhost;dbname=users", 'root', '');  
+          $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+          // $DBH->prepare('SELECT user_login FROM users')->execute();  
+        }  
+        catch(PDOException $e) {  
+            file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);  
+            return $err = "Houston, we have a problem.";  
+        }
+	}
 	
 	// метод выборки данных
 	public function get_data()
