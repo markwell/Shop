c00fbc3cb493cc9334d4d4d7e31a446533c83961
Mark <mrak.mk@ya.ru>
2015-01-09 05:30:20 +1000

    Отображение товаров

--- application/views/items_view.php
+++ application/views/items_view.php
@@ -1,3 +1,18 @@
+<div class="container">
+	<div class="row">
+		<div class="media">
+		  <a class="media-left media-middle" href="#">
+		    <img data-src="/shop/images/1.jpg" alt="...">
+		  </a>
+		  <div class="media-body">
+		    <h4 class="media-heading">Middle aligned media</h4>
+		    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+
+		    Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+		  </div>
+		</div>
+	</div>
+</div>
 <div class="container text-center">
 	<div class="row">
 		<nav>
--- application/views/template_view.php
+++ application/views/template_view.php
@@ -77,9 +77,9 @@
 	    </div>	  	  
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="js/jquery.min.js"></script>
+		<script src="/shop/js/jquery.min.js"></script>
 	  <!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="js/bootstrap.js"></script>
+		<script src="/shop/js/bootstrap.js"></script>
 	 
    </body>
 </html>
