9d4fc81cfc03c6dd83dd6a03a6d553dc49f26172
Mark <mrak.mk@ya.ru>
2015-01-17 22:32:16 +0300

    Исправил тип на логин

--- application/views/register_view.php
+++ application/views/register_view.php
@@ -5,7 +5,7 @@
 		<form role="form" method="POST" action="/shop/register/newuser">
 		  <div class="form-group">
 		    <label for="exampleInputEmail1">Логин</label>
-		    <input type="email" class="form-control" id="exampleInputEmail1" name="login" placeholder="Enter login" required>
+		    <input type="login" class="form-control" id="exampleInputEmail1" name="login" placeholder="Enter login" required>
 		  </div>
 		  <div class="form-group">
 		    <label for="exampleInputPassword1">Пароль</label>
